7bc99ecec6f06eb37cff2c7c356fd86b447bbb4e
SeanCushnie <133601523+SeanCushnie@users.noreply.github.com>
2023-07-15 16:47:44 +0100

    changed css for Footer and App.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,22 +1,19 @@
-import React from 'react'
-import "./Header.css";
-import ReactDOM from 'react-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faMagnifyingGlass, faGlobe } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import "./Footer.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faMagnifyingGlass, faGlobe } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <div className='Footer'>
-        
-        <div className='icon'>
-          <Link to='library'><FontAwesomeIcon icon={faHouse} size='3x' /> Home</Link>
-          <Link to='browse'><FontAwesomeIcon icon={faGlobe} size='3x' />Browse</Link>
-          <Link to='search'><FontAwesomeIcon icon={faMagnifyingGlass} size='3x' />Search</Link>
-        </div>
-        
+      <div className='icon'>
+        <Link to='library'><FontAwesomeIcon icon={faHouse} size='3x' /> </Link>
+        <Link to='browse'><FontAwesomeIcon icon={faGlobe} size='3x' /></Link>
+        <Link to='search'><FontAwesomeIcon icon={faMagnifyingGlass} size='3x' /></Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
