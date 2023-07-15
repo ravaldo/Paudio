b8b1ed27c3a29e0937fc03b51d8c49db1c5a0268
SeanCushnie <133601523+SeanCushnie@users.noreply.github.com>
2023-07-15 17:41:28 +0100

    changed css and testing to see if this breaks

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
+    const greeting = new Date();
+    const hrs = greeting.getHours();
 
-    
+    let greet;
+
+    if (hrs >= 0 && hrs < 12) {
+        greet = 'Good morning';
+    } else if (hrs >= 12 && hrs < 18) {
+        greet = 'Good afternoon';
+    } else {
+        greet = 'Good evening';
+    }
+
     return (
         <div className='Header'>
-
-            <h1>Header</h1>
-           
-
+            <h1>{greet}</h1>
         </div>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
