6b867bca78f0ed52955ad7a0e3dca92d1c464d7f
Kirsty <kirstmcbride@gmail.com>
2023-07-16 14:03:39 +0100

    shortened descriptions on browse cards

--- client/src/components/BrowseCard.js
+++ client/src/components/BrowseCard.js
@@ -6,12 +6,16 @@ const BrowseCard = ({ podcastSeries }) => {
 
     if (!podcastSeries)
         return null;
+
+    const podcastDescription = podcastSeries.description;
+    const shortDescription = podcastDescription.split(' ').slice(0,18).join(' ')
+
     return (
         <div className='BrowseCard'>
-            <h4 classname="podcastName">{podcastSeries.name}</h4>
-            <img classname="podcastImage" src={podcastSeries.imageUrl} alt={`This is the cover image for ${podcastSeries.name}`} />
-            <h6 classname="podcastDescription">{podcastSeries.description}</h6>
-            <h6 classname="pocastTotalEps">{podcastSeries.totalEpisodesCount}</h6>
+            <h4 className="podcastName">{podcastSeries.name}</h4>
+            <img className="podcastImage" src={podcastSeries.imageUrl} alt={`This is the cover image for ${podcastSeries.name}`} />
+            <h6 className="podcastDescription">{shortDescription}...</h6>
+            <h6 className="pocastTotalEps">{podcastSeries.totalEpisodesCount}</h6>
         </div>
     )
 }
