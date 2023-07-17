893c8339b8b0e7a545c1fdadc6526350a0f77bcb
SeanCushnie <133601523+SeanCushnie@users.noreply.github.com>
2023-07-17 13:24:23 +0100

    Added toggle to header, replacing bulb icon  and changed css for BrowserCard.

--- client/src/components/BrowseCard.js
+++ client/src/components/BrowseCard.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import './BrowseCard.css'
 
-const BrowseCard = ({ podcastSeries }) => {
+const BrowseCard = ({ podcastSeries, lightDark }) => {
 
     const shortDescription = podcastSeries.description.split(' ').slice(0,18).join(' ');
 
     if (!podcastSeries)
         return null;
     return (
-        <div className='BrowseCard'>
-            <h4 classname="podcastName">{podcastSeries.name}</h4>
-            <img classname="podcastImage" src={podcastSeries.imageUrl} alt={`This is the cover image for ${podcastSeries.name}`} />
-            <h6 classname="podcastDescription">{podcastSeries.description}</h6>
-            <h6 className="pocastTotalEps">{podcastSeries.totalEpisodesCount} Episodes</h6>
+        <div className={`BrowseCard ${lightDark}`}>
+            <img classame="podcastImage" src={podcastSeries.imageUrl} alt={`This is the cover image for ${podcastSeries.name}`} />
+            <div classname ="podcastDetails">
+                <h4 classname="podcastName">{podcastSeries.name}</h4>
+                <h6 className="pocastTotalEps">{podcastSeries.totalEpisodesCount} Episodes</h6>
+                <h6 classname="podcastDescription">{shortDescription}...</h6>
+            </div>
         </div>
     )
 }
--- client/src/components/toggle.js
+++ client/src/components/toggle.js
@@ -2,11 +2,29 @@
 import React from 'react'
 import './toggle.css'
 
-const Toggle = () => (
-  <label class="switch">
-    <input type="checkbox" />
-    <span class="slider round"></span>
-  </label>
-)
+const Toggle = ({onClick}) => {
+
+  return (
+    <label class="switch">
+      <input type="checkbox" onClick={onClick} />
+      <span class="slider round"></span>
+    </label>
+  )
+}
+
+// function activateButton(button) { // You missed this part
+//   button.addEventListener('click', clickHandler);
+// }
+// function disableButton(button) { // You missed this part
+//   button.removeEventListener('click', clickHandler);
+// }
+
+// function toggleButtons(value, button) {    
+//   if (value === 1) {
+//       activateButton(button);  
+//   } else {
+//       disableButton(button); 
+//   }
+
 
 export default Toggle;
