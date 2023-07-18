c7da032a12e05121da329543870b9e2eac38f650
SeanCushnie <133601523+SeanCushnie@users.noreply.github.com>
2023-07-18 14:02:59 +0100

    test

--- client/src/components/PlayListCard.js
+++ client/src/components/PlayListCard.js
@@ -6,11 +6,12 @@ import './PlayListCard.css'
 const PlayListCard = ({ episode, deleteFromPlaylist, lightDark, defaultImgUrl }) => {
     const handleClick = () => {
         deleteFromPlaylist(episode)
-    }
+     }
 
-
+ 
     return (
         <div>
+        
             <div className={`PlaylistCard ${lightDark}`}>
                 <img src={episode.imageUrl ? episode.imageUrl : defaultImgUrl} alt={`image of ${episode.name}`} />
                 <h1> {episode.name} </h1>
